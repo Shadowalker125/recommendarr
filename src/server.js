ae082d65b49378a646fa231b46aa27b1b350639e
shadowalker125 <16189570+Shadowalker125@users.noreply.github.com>
2025-02-16 23:51:33 -0600

    dockerized

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ fastify.register(artistRoutes)
 
 const start = async () => {
     try {
-        await fastify.listen({ port: 3000, host: '0.0.0.0' })
+        await fastify.listen({ port: 4500, host: '0.0.0.0' })
     } catch (err) {
         fastify.log.error(err)
         process.exit(1)
