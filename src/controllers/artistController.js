ac5d748f6e3fe5537d55da9d71a63f7177b81698
shadowalker125 <16189570+Shadowalker125@users.noreply.github.com>
2025-03-04 17:21:48 -0600

    Moved the validateEnv() function, not sure this is the best place but it works

--- src/controllers/artistController.js
+++ src/controllers/artistController.js
@@ -1,7 +1,9 @@
 import fetchArtistMusicBrainzID from "../services/fetchArtistMusicBrainzID.js";
 import fetchData from "../services/fetchData.js";
+import {validateEnv} from "../config/config.js";
 
 export async function getArtistMusicBrainzId(request, reply) {
+    validateEnv();
     try {
         const ArtistMusicBrainzIDs = await fetchArtistMusicBrainzID(fetchData);
         reply.send(ArtistMusicBrainzIDs);
